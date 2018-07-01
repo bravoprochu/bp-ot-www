bfa56de8a653915093d500b5630cc2c18a95b21e
Bartosz Prochowski <bravoprochu@gmail.com>
2018-07-01 22:59:08 +0200

    paymentTerms cleanUp

--- src/app/shared/payment-terms/payment-terms/payment-terms.component.ts
+++ src/app/shared/payment-terms/payment-terms/payment-terms.component.ts
@@ -2,7 +2,7 @@ import { OnDestroy, ViewChild, AfterViewInit } from '@angular/core';
 import { Component, Input, OnInit } from '@angular/core';
 import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { Observable, Subject } from 'rxjs';
-import { MatSelect } from '@angular/material';
+import { MatSelect, MatDatepickerInputEvent } from '@angular/material';
 import { isDate, isNumber } from 'util';
 import { PaymentTermsService } from '../payment-terms.service';
 import { IPaymentTerm } from '../i-payment-term';
@@ -17,13 +17,13 @@ import { takeUntil } from 'rxjs/operators';
   styleUrls: ['./payment-terms.component.css']
 })
 export class PaymentTermsComponent implements OnInit, OnDestroy {
-
+  @Input() rForm: FormGroup; //formPaymentTermsGroup
+  @ViewChild(MatSelect) matSelect: MatSelect;
   
+ 
   ngOnDestroy(): void {
     this.isDestroyed$.next(true);
   }
-  @Input() rForm: FormGroup; //formPaymentTermsGroup
-  @ViewChild(MatSelect) matSelect: MatSelect;
 
   constructor(
     private cf: PaymentTermsService,
@@ -32,13 +32,7 @@ export class PaymentTermsComponent implements OnInit, OnDestroy {
 
   ngOnInit() {
     this.isDestroyed$ = new Subject<boolean>();
-    //
-    // setting options
-    //
-
-    
     this.paymentTermsArr = this.cf.getPaymentTerms();
-    // this.initComponentObservable();
   }
 
 
@@ -48,30 +42,9 @@ export class PaymentTermsComponent implements OnInit, OnDestroy {
   paymentTermsArr: IPaymentTerm[];
 
 
-  // initComponentObservable() {
-  //   this.formDay0.valueChanges.pipe(
-  //     takeUntil(this.isDestroyed$)
-  //   )
-  //     .subscribe(
-  //       (_data: any) => {
-  //         this.formatDay0();
-  //       },
-  //   )
-  // }
-
   compareWith(obj1: IPaymentTerm, obj2: IPaymentTerm) {
     return obj2 ? obj1.paymentTermId == obj2.paymentTermId : null;
   }
-
-
-  // formatDay0() {
-  //   let _minDate: moment.Moment = moment(this.rForm.get('day0').value);
-  //   let d = _minDate.format(DEFAULT_APP_VALUES.dateLocalFormat);
-  //   this.minDate = _minDate;
-  // }
-
-
-
 
 
   //#region rForm getters
@@ -104,12 +77,6 @@ export class PaymentTermsComponent implements OnInit, OnDestroy {
   get paymentTerm() {
     return this.rForm.get('paymentTerm');
   }
-
-  // get minDate():string {
-  //   let _day0= this.rForm.get('day0');
-  //   return _day0 && (_day0.value.isValid()) ? (_day0.value).format(DEFAULT_APP_VALUES.dateLocalFormat) : this.cf.mc.getFormatedDate();
-  // }
-
   //#endregion
 
 }
