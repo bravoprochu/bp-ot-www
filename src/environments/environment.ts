940a73d4ddb99691095202df72492b58f1ac3144
Bartosz Prochowski <bravoprochu@gmail.com>
2021-01-12 17:07:17 +0100

    fix dev server addr

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,8 @@
 // `ng build --env=prod` then `environment.prod.ts` will be used instead.
 // The list of which env maps to which file can be found in `.angular-cli.json`.
 
- const server='https://offertrans-sped-api.azurewebsites.net/'
-// const server='https://localhost:44315/';
+ // const server='https://offertrans-sped-api.azurewebsites.net/'
+const server='http://localhost:5000/';
 
 
 export const environment = {
