157e150ade5523e043e2f9dc905833e9a7d624d0
Bartosz Prochowski <bravoprochu@gmail.com>
2021-03-05 19:10:34 +0100

    pending component fix min-height

--- src/app/shared/pending/pending.component.ts
+++ src/app/shared/pending/pending.component.ts
@@ -1,15 +1,15 @@
-import { Component, OnInit } from '@angular/core';
+import { Input } from "@angular/core";
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-pending',
-  templateUrl: './pending.component.html',
-  styleUrls: ['./pending.component.css']
+  selector: "app-pending",
+  templateUrl: "./pending.component.html",
+  styleUrls: ["./pending.component.css"],
 })
 export class PendingComponent implements OnInit {
+  @Input() isPending = false;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
