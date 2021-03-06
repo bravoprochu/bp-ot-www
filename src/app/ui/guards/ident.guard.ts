c7847110c6b1c7ce4c231e2a36095006ba585657
Bartosz Prochowski <bravoprochu@gmail.com>
2021-03-07 00:46:14 +0100

    canActivate identGuard rxWay ifAuthorised

--- src/app/ui/guards/ident.guard.ts
+++ src/app/ui/guards/ident.guard.ts
@@ -1,36 +1,48 @@
-import { observable } from 'rxjs/symbol/observable';
-import { LoginComponent } from '../../auth/login/login.component';
-import { TokenService } from '../../services/token.service';
-import { Injectable } from '@angular/core';
-import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, Router } from '@angular/router';
-import { Observable } from 'rxjs/Observable';
-import 'rxjs/add/observable/of';
-import 'rxjs/add/operator/do';
-import 'rxjs/add/operator/map';
+import { LoginComponent } from "../../auth/login/login.component";
+import { TokenService } from "../../services/token.service";
+import { Injectable } from "@angular/core";
+import {
+  CanActivate,
+  ActivatedRouteSnapshot,
+  RouterStateSnapshot,
+  Router,
+} from "@angular/router";
+import { Observable } from "rxjs/Observable";
+import "rxjs/add/observable/of";
+import "rxjs/add/operator/do";
+import "rxjs/add/operator/map";
 import { MatDialog } from "@angular/material";
 
+import { of } from "rxjs";
+import { switchMap } from "rxjs/operators";
 
 @Injectable()
 export class IdentGuard implements CanActivate {
-constructor(
-  private tokenService: TokenService,
-  private router: Router,
-  private dialog: MatDialog
- ){}
+  constructor(
+    private tokenService: TokenService,
+    private router: Router,
+    private dialog: MatDialog
+  ) {}
 
   canActivate(
     next: ActivatedRouteSnapshot,
-    state: RouterStateSnapshot):Observable<boolean>|boolean {
-      let res=false;
-      if(this.tokenService.isAuthorized(next.data)){
-        return true
-      } else {
-        this.dialog.open(LoginComponent)
-          .afterClosed()
-          .take(1)
-          .subscribe(s=>res=s);
-      }
-
-      return res;
+    state: RouterStateSnapshot
+  ): Observable<boolean> | boolean {
+    let res = false;
+    if (this.tokenService.isAuthorized(next.data)) {
+      return of(true);
+    } else {
+      return this.dialog
+        .open(LoginComponent)
+        .afterClosed()
+        .pipe(
+          switchMap((isLoggedIn: boolean) => {
+            return isLoggedIn === true &&
+              this.tokenService.isAuthorized(next.data)
+              ? of(true)
+              : of(false);
+          })
+        );
+    }
   }
 }
