a72552141aee66046be3b73d150d365fbbabbf2f
Bartosz Prochowski <bravoprochu@gmail.com>
2021-03-07 22:43:47 +0100

    dragDropOption fix component location

--- src/app/other-modules/drag-drop-options/drag-drop-options.module.ts
+++ src/app/other-modules/drag-drop-options/drag-drop-options.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
-import { TableDragDropOptionsComponent } from "@bpShared/table-drag-drop-options/table-drag-drop-options.component";
 import { FlexLayoutModule } from "@angular/flex-layout";
 import { MatExpansionModule, MatIconModule } from "@angular/material";
 import { DragDropModule } from "@angular/cdk/drag-drop";
+import { TableDragDropOptionsComponent } from "./components/table-drag-drop-options/table-drag-drop-options.component";
 
 const IMPORT_EXPORT_MODULES = [
   FlexLayoutModule,
