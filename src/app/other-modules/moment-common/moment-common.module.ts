983a9df169608b6021fa718c751c0ab0495dd98a
Bartosz Prochowski <bravoprochu@gmail.com>
2021-03-07 22:41:08 +0100

    otherModule momentCommon

---
+++ src/app/other-modules/moment-common/moment-common.module.ts
@@ -0,0 +1,10 @@
+import { NgModule } from "@angular/core";
+import { CommonModule } from "@angular/common";
+import { MomentCommonService } from "./services/moment-common.service";
+
+@NgModule({
+  imports: [CommonModule],
+  declarations: [],
+  providers: [MomentCommonService],
+})
+export class MomentCommonModule {}
---
+++ src/app/other-modules/moment-common/services/moment-common.service.ts
@@ -0,0 +1,147 @@
+import { Injectable } from "@angular/core";
+import * as moment from "moment";
+import { DEFAULT_APP_VALUES } from "environments/environment";
+import { IDateRange } from "@bpCommonInterfaces/i-date-range";
+
+@Injectable()
+export class MomentCommonService {
+  constructor() {}
+
+  convertToConstTime(date: any): moment.Moment {
+    let res = undefined;
+    if (moment(date).isValid()) {
+      res = moment(date);
+      this.setConstTimeMoment(res);
+      return moment(res);
+    } else {
+      res = moment();
+      this.setConstTimeMoment(res);
+      return moment(res);
+    }
+  }
+
+  convertToMoment(date: string) {
+    let d = moment(date);
+    return d.isValid ? d : moment();
+  }
+
+  dateLocaleFormat(): string {
+    return "YYYY-MM-DD";
+  }
+
+  dateTimeLocaleFormat(): string {
+    return "YYYY-MM-DDTHH:mm";
+  }
+
+  getNextHour(hoursToAdd: number = 1) {
+    return moment()
+      .add(hoursToAdd, "hours")
+      .minutes(0)
+      .format(this.dateTimeLocaleFormat());
+  }
+
+  getTodayConstTimeFormated(): string {
+    let todayConstTime = moment();
+    this.setConstTimeMoment(todayConstTime);
+    return todayConstTime.format(DEFAULT_APP_VALUES.dateLocalFormat);
+  }
+
+  getToday(): moment.Moment {
+    let res = moment();
+    this.setConstTimeMoment(res);
+    return res;
+  }
+
+  getTodayFormated(): string {
+    return this.getFormatedDate();
+  }
+
+  getPreviousMonth(): IDateRange {
+    return {
+      dateStart: this.getToday()
+        .subtract(1, "month")
+        .date(1)
+        .hour(0)
+        .minute(0)
+        .millisecond(0),
+      dateEnd: moment(moment().date(1))
+        .subtract(1, "day")
+        .hour(23)
+        .minute(59)
+        .millisecond(999),
+    } as IDateRange;
+  }
+
+  getNow(): moment.Moment {
+    return moment();
+  }
+
+  getNowFormated(): string {
+    return this.getNow().format(
+      `${DEFAULT_APP_VALUES.dateLocalFormat}THH:mm:ss`
+    );
+  }
+
+  getTodayConstTimeMoment(): moment.Moment {
+    let res = this.getToday();
+    this.setConstTimeMoment(res);
+    return res;
+  }
+
+  getFormatedDate(date?: moment.Moment): string {
+    return date
+      ? date.format(DEFAULT_APP_VALUES.dateLocalFormat)
+      : this.getToday().format(DEFAULT_APP_VALUES.dateLocalFormat);
+  }
+
+  isCurrencyNbpValidDate(date: moment.Moment): moment.Moment {
+    if (date instanceof moment) {
+      let hourBefor: number = 13;
+      let format = DEFAULT_APP_VALUES.dateLocalFormat;
+      let today = moment().format(format);
+      let calDate = date.format(format);
+
+      //date is greater than today
+      if (calDate > today) {
+        date = date.subtract(date.diff(moment(), "day"), "day");
+      }
+      //calendar date is today, and its before 13:00 (nbp emits new rates)
+      if (calDate == today && date.hour() < hourBefor) {
+        date = date.subtract(1, "day");
+      }
+      //it is weekend day
+      if (date.day() == 0 || date.day() == 6) {
+        date =
+          date.day() == 0 ? date.subtract(2, "day") : date.subtract(1, "day");
+      }
+    }
+    return date;
+  }
+
+  isDate(val: any): boolean {
+    return moment.isDate(new Date(val));
+  }
+
+  setConstTimeMoment(date: moment.Moment, hour?: number): void {
+    //date = date ? moment(date) : moment();
+    hour = hour ? hour : DEFAULT_APP_VALUES.defaultConstHour;
+
+    date.set({
+      hour: hour,
+      hours: hour,
+      minute: 0,
+      minutes: 0,
+      second: 0,
+      seconds: 0,
+      millisecond: 0,
+      milliseconds: 0,
+    });
+  }
+
+  setFormatedDateTime(date: any): string {
+    if (date == null || !moment(date).isValid) {
+      return null;
+    }
+    return moment(date).utc(false).format(this.dateTimeLocaleFormat());
+  }
+}
